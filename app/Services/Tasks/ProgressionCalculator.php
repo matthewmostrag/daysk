2604328cf85ddcbcdcbbb0d4c8de4089e017c1ab
Matthew Mostrag <matthew.mostrag@gmail.com>
2017-12-09 21:48:38 +0100

    corrections

--- app/Services/Tasks/ProgressionCalculator.php
+++ app/Services/Tasks/ProgressionCalculator.php
@@ -11,10 +11,14 @@ class ProgressionCalculator
     {
         $today = Carbon::today();
 
-        // @todo replace that with a getTodayTasks method somewhere
+        // @todo replace that with a getTodayTasks method somewhere : this needs to be a scope
         $todayTasks = Task::whereDate('due_date', $today->toDateString())->get();
         $tasksCount = $todayTasks->count();
         $tasksDone = $todayTasks->where('status', 1)->count();
+
+        if ($tasksCount === 0) {
+            return 0;
+        }
 
         return $tasksDone / $tasksCount * 100;
     }
